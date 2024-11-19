45e79254f21a2d248fe5967ccc0480ff9977b8c1
ArchieJP <archiejp1@gmail.com>
2024-11-19 21:01:13 +0000

    added code for sales program

--- Task B_Trikommerce Project/sales.py
+++ Task B_Trikommerce Project/sales.py
@@ -9,4 +9,9 @@ def daily_sales(available_items, current_day):
     :param current_day: Current day of the simulation
     :return: Sold units and updated available items
     """
-  
+    if current_day % 7 != 0:  # Sales occur only on non-restocking days
+        sold_units = random.randint(0, min(200, available_items))
+        available_items -= sold_units
+    else:
+        sold_units = 0
+    return sold_units, available_items
